b4fcbeebaa36d0d1006df4dfe6af30a377941ec1
juan jose cano <juan.nal.743@gmail.com>
2022-05-05 17:48:29 -0500

    agregamospaginavideojuego

---
+++ js/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyADagITjhBFpQD0c__coRoscTbgxpF0iu8",
+    authDomain: "videojuegocesde.firebaseapp.com",
+    projectId: "videojuegocesde",
+    storageBucket: "videojuegocesde.appspot.com",
+    messagingSenderId: "694736016453",
+    appId: "1:694736016453:web:160088b7c08ea62ebb3f2c",
+    measurementId: "G-6BNE5EFCNC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
